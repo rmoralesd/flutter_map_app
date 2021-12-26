e83addc1bcaa5a5a444413d1a0fd56e969192e71
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-12-26 11:16:26 -0500

    Establecer estilo del mapa

--- lib/blocs/map/map_bloc.dart
+++ lib/blocs/map/map_bloc.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:maps_app/themes/themes.dart';
 
 part 'map_event.dart';
 part 'map_state.dart';
@@ -13,6 +16,7 @@ class MapBloc extends Bloc<MapEvent, MapState> {
 
   void _onInitMap(OnMapInitializedEvent event, Emitter<MapState> emit) {
     _mapController = event.controller;
+    _mapController!.setMapStyle(jsonEncode(uberMapTheme));
     emit(state.copyWith(isMapInitialized: true));
   }
 }
