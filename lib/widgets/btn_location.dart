3c2efb5ecf89ee681c45a7a6f6438356bd211867
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-12-26 11:57:00 -0500

    Adición de customSnackbar

--- lib/widgets/btn_location.dart
+++ lib/widgets/btn_location.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:maps_app/blocs/blocs.dart';
+import 'package:maps_app/ui/custom_snackbar.dart';
 
 class BtnCurrentLocation extends StatelessWidget {
   const BtnCurrentLocation({Key? key}) : super(key: key);
@@ -21,7 +22,11 @@ class BtnCurrentLocation extends StatelessWidget {
           ),
           onPressed: () {
             final userLocation = locationBloc.state.lastKnownLocation;
-            if (userLocation == null) return;
+            if (userLocation == null) {
+              final snack = CustomSnackbar(message: 'No hay ubicación');
+              ScaffoldMessenger.of(context).showSnackBar(snack);
+              return;
+            }
             mapBloc.moveCamera(userLocation);
           },
         ),
