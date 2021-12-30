cd8631338be94348d90d8251e16bed4a07176ccb
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-12-30 06:54:18 -0500

    Creación de SearchBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/blocs/blocs.dart';
 import 'package:maps_app/blocs/gps/gps_bloc.dart';
 import 'package:maps_app/blocs/location/location_bloc.dart';
 import 'package:maps_app/blocs/map/map_bloc.dart';
@@ -7,16 +8,12 @@ import 'package:maps_app/screens/screens.dart';
 
 void main() {
   runApp(MultiBlocProvider(providers: [
+    BlocProvider(create: (context) => GpsBloc()),
+    BlocProvider(create: (context) => LocationBloc()),
     BlocProvider(
-      create: (context) => GpsBloc(),
-    ),
-    BlocProvider(
-      create: (context) => LocationBloc(),
-    ),
-    BlocProvider(
-      create: (context) =>
-          MapBloc(locationBloc: BlocProvider.of<LocationBloc>(context)),
-    )
+        create: (context) =>
+            MapBloc(locationBloc: BlocProvider.of<LocationBloc>(context))),
+    BlocProvider(create: (context) => SearchBloc()),
   ], child: const MapsApp()));
 }
 
