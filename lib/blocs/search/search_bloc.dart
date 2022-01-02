54d21b6b541a0bfcfbaf3420ceaa36f3298d86c6
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2022-01-02 14:35:09 -0500

    Decodificacion de la Polyline

--- lib/blocs/search/search_bloc.dart
+++ lib/blocs/search/search_bloc.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:maps_app/models/models.dart';
 import 'package:maps_app/services/service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
@@ -20,8 +22,23 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
   }
 
   Future getCoorsStartToEnd(LatLng start, LatLng end) async {
-    final resp = await trafficService.getCoordsStartToEnd(start, end);
+    final trafficResponse =
+        await trafficService.getCoordsStartToEnd(start, end);
 
-    return resp;
+    final distance = trafficResponse.routes[0].distance;
+    final duration = trafficResponse.routes[0].duration;
+
+    final geometry = trafficResponse.routes[0].geometry;
+    final points = decodePolyline(geometry, accuracyExponent: 6);
+
+    final latLngList = points
+        .map((coor) => LatLng(coor[0].toDouble(), coor[1].toDouble()))
+        .toList();
+
+    return RouteDestination(
+      points: latLngList,
+      duration: duration,
+      distance: distance,
+    );
   }
 }
