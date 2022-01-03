f4c0c4aae89999251caaaff1301b07e2c5b6f11a
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2022-01-03 05:39:54 -0500

    Configuracion inicial de servicio para busqueda de lugares en MapBox

--- lib/services/places_interceptor.dart
+++ lib/services/places_interceptor.dart
@@ -1,16 +1,15 @@
 import 'package:dio/dio.dart';
 
-class TrafficInterceptor extends Interceptor {
+class PlacesInterceptor extends Interceptor {
   final accessToken =
       'pk.eyJ1Ijoicm1vcmFsZXM4MzAzIiwiYSI6ImNreHdlb3diYzZyY2syd3BmbTZtajlrOHoifQ.idpbyOF61CqiFELrfG618A';
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     options.queryParameters.addAll({
-      'alternatives': true,
-      'geometries': 'polyline6',
-      'overview': 'simplified',
-      'steps': false,
-      'access_token': accessToken
+      'access_token': accessToken,
+      'language': 'es',
+      'limit': 7,
     });
     super.onRequest(options, handler);
   }
--- lib/services/traffic_service.dart
+++ lib/services/traffic_service.dart
@@ -5,10 +5,14 @@ import 'package:maps_app/services/service.dart';
 
 class TrafficService {
   final Dio _dioTraffic;
+  final Dio _dioPlaces;
   final String _basicTrafficUrl = 'https://api.mapbox.com/directions/v5/mapbox';
+  final String _basicPlacesUrl =
+      'https://api.mapbox.com/geocoding/v5/mapbox.places';
 
   TrafficService()
-      : _dioTraffic = Dio()..interceptors.add(TrafficInterceptor());
+      : _dioTraffic = Dio()..interceptors.add(TrafficInterceptor()),
+        _dioPlaces = Dio()..interceptors.add(PlacesInterceptor());
 
   Future<TrafficResponse> getCoordsStartToEnd(LatLng start, LatLng end) async {
     final coorsString =
@@ -20,5 +24,16 @@ class TrafficService {
     final data = TrafficResponse.fromMap(resp.data);
 
     return data;
+  }
+
+  Future getResultsByQuery(LatLng proximity, String query) async {
+    if (query.isEmpty) return [];
+    final url = '$_basicPlacesUrl/$query.json';
+
+    final resp = await _dioPlaces.get(url, queryParameters: {
+      'proximity': '${proximity.longitude},${proximity.latitude}'
+    });
+
+    return [];
   }
 }
